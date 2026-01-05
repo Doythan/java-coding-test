af0876f7361f808470f2ff61561dc23242952dfb
Doythan <won03289@gmail.com>
2026-01-05 16:39:01 +0900

    feat(sort): 회의실 배정 문제 리팩토링

--- sort/S1931/Main.java
+++ sort/S1931/Main.java
@@ -15,17 +15,22 @@ public class Main {
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr, (a, b) -> a[1] == b[1] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
+        System.out.println(getMaxMeetingCount(arr));
+        
+    }
 
-        int answer = 1;
-        int idx = 0;
-        for (int i=1; i<n; i++) {
-            if (arr[idx][1] <= arr[i][0]) {
-                answer ++;
-                idx = i;
+    private static int getMaxMeetingCount(int[][] meetings) {
+        Arrays.sort(meetings, (a, b) -> a[1] == b[1] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
+
+        int cnt = 1;
+        int lastIdx = 0;
+
+        for (int i=1; i<meetings.length; i++) {
+            if (meetings[lastIdx][1] <= meetings[i][0]) {
+                cnt ++;
+                lastIdx = i;
             }
         }
-
-        System.out.println(answer);
+        return cnt;
     }
 }
