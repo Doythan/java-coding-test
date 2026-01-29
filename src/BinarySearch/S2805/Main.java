1f7e2351519ab948df9da24ce3cd2efeb7c2dcd0
Doythan <won03289@gmail.com>
2026-01-29 17:54:08 +0900

    feat: 나무자르기 문제 해결

--- src/BinarySearch/S2805/Main.java
+++ src/BinarySearch/S2805/Main.java
@@ -1,59 +1,59 @@
-package src.BinarySearch.S2805;
+package src.binarysearch.S2805;
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
+    static int N;
+    static long M; // M도 long으로 받는 것이 안전합니다.
     static int[] treeHeight;
-    static int answer = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        M = Long.parseLong(st.nextToken());
         treeHeight = new int[N];
 
+        int max = 0;
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < N; i++) {
             treeHeight[i] = Integer.parseInt(st.nextToken());
+            if (treeHeight[i] > max)
+                max = treeHeight[i];
+        }
 
-        Arrays.sort(treeHeight);
+        // Arrays.sort는 필수가 아니지만 하셔도 무방합니다.
+        // 최댓값만 알면 이분 탐색이 가능하므로 max값만 찾아도 됩니다.
 
-        getHeight();
+        System.out.println(getHeight(max));
     }
 
-    static void getHeight() {
-        int left = 0;
-        int right = treeHeight[N - 1];
+    static long getHeight(int max) {
+        long left = 0;
+        long right = max;
+        long answer = 0;
 
         while (left <= right) {
-            int temp = 0;
-            int mid = left + (right - left) / 2;
+            long mid = (left + right) / 2;
+            long sum = 0; // 반드시 long 타입 사용!
 
-            for (int i = 0; i < N; i++) {
-                if (treeHeight[i] > mid) {
-                    temp += treeHeight[i] - mid;
+            for (int h : treeHeight) {
+                if (h > mid) {
+                    sum += (h - mid);
                 }
             }
 
-            if (temp == M) {
-                if (answer < mid)
-                    answer = mid;
-                continue;
+            // 자른 나무의 합이 목표치보다 크거나 같다면 (성공)
+            if (sum >= M) {
+                answer = mid; // 일단 답으로 기록하고
+                left = mid + 1; // 더 높이 자를 수 있는지 확인 (오른쪽 탐색)
+            } else {
+                // 목표치보다 부족하다면 (실패)
+                right = mid - 1; // 더 낮게 잘라야 함 (왼쪽 탐색)
             }
-
-            if (mid < M) {
-                right = mid - 1;
-                continue;
-            }
-
-            if (mid > M)
-                left = mid + 1;
-
         }
-        System.out.println(answer);
+        return answer;
     }
 }
